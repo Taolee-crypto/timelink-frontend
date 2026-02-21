29122e1d7bc3785d2f575b952c7bdf69e962bd58
Timelink <85010918+Taolee-crypto@users.noreply.github.com>
2026-02-21 22:50:08 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import App from './App.tsx'
-import Create from './Create.tsx'  // 아래에서 만들 파일
-import './index.css'
+// ... 기존 import
+import Create from './Create.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/create" element={<Create />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+// Routes 안에 추가
+<Route path="/create" element={<Create />} />
